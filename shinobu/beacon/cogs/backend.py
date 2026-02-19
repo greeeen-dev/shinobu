874b5e2b3a9c0d5575ebd93433080ea9076fb7c9
green. <41323182+greeeen-dev@users.noreply.github.com>
2026-02-19 18:35:49 +0000

    Fix backend cog init logic

--- shinobu/beacon/cogs/backend.py
+++ shinobu/beacon/cogs/backend.py
@@ -2,11 +2,10 @@ from discord.ext import commands
 from shinobu.runtime.models import shinobu_cog
 
 class BeaconBackend(shinobu_cog.ShinobuCog):
-    def __init__(self, bot, **kwargs):
+    def __init__(self, bot):
         # Register cog metadata
-        self.setup_shinobu_cog(
+        super().__init__(
             bot,
-            **kwargs,
             shinobu_metadata=shinobu_cog.ShinobuCogMetadata(
                 name="Beacon Backend",
                 description="Manages the Shinobu Beacon bridge protocol.",
@@ -16,7 +15,7 @@ class BeaconBackend(shinobu_cog.ShinobuCog):
 
     def on_entitlements_issued(self):
         # We will initialize Beacon here
-        self.bot.shared_objects.add("beacon")
+        # self.bot.shared_objects.add("beacon")
         return
 
 def get_cog_type():
